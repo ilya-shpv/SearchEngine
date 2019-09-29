c154f9a61a00afcf80d70632530093f1ed286dc5
Ilya <ilya.shpv@gmail.com>
2019-09-29 20:25:03 +0300

    Initial commit

---
+++ src/main/java/com/ilya/searchEngineDemo/SearchEngineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ilya.searchEngineDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchEngineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilya/searchEngineDemo/SearchEngineDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ilya.searchEngineDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchEngineDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
